e3ce4aed68124bed40beb4b61b03d684aa114942
Marta Hendel <marta.hendel@gmail.com>
2019-03-14 15:12:50 +0000

    created Navbar component

---
+++ src/components/Navigations/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Navbar = props => (
+  <nav className="navbar navbar-default navbar-fixed-top navbar-custom">
+    <div className="container">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="/">
+          ReactJS Academy
+        </a>
+      </div>
+      <ul className="nav navbar-nav pull-right">
+        <li className="hidden-xs">
+          <a href="#about">About us</a>
+        </li>
+        <li>
+          <button onClick={props.toggleMenu} className="btn btn-lg btn-outline">
+            <i className="fa fa-graduation-cap" /> <span>Training</span>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+
+Navbar.propTypes = {
+  toggleMenu: PropTypes.func.isRequired
+}
+
+export default Navbar
